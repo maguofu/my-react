a26b901870f3ff480dbb71c7e3fd6f68099b3b62
maguofu <maguofu@sfmail.sf-express.com>
2021-02-20 15:58:00 +0800

    [update]修改文件名

--- src/pages/test/main.tsx
+++ src/pages/test/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
-import { store } from './redux/state';
+import { store } from './redux/store';
 import App from './views/index'
 
 ReactDOM.render(
